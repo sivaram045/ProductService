12deedafcfbad4f49e20ad20508f312962cda9c5
Siva <sivaram045@gmail.com>
2024-03-17 04:53:04 +0530

    put and post methods

--- src/main/java/com/ecomapp/productservice/controllers/ProductController.java
+++ src/main/java/com/ecomapp/productservice/controllers/ProductController.java
@@ -1,9 +1,7 @@
 package com.ecomapp.productservice.controllers;
 
 import com.ecomapp.productservice.models.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -16,5 +14,14 @@ public class ProductController {
         return products;
     }
 
+    @PostMapping()
+    public Product addNewProduct(@RequestBody Product product) {
+        return new Product();
+    }
+
+    @PutMapping("/{id}")
+    public Product replaceProduct (@PathVariable long id, @RequestBody Product product) {
+        return new Product();
+    }
 
 }
