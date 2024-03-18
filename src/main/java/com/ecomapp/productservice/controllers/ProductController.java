2a747ce69373201dccb11c9b302ef8390393fdec
sivaram045 <sivaram045@gmail.com>
2024-03-18 06:17:18 +0530

    get single product from fake store API - successful

--- src/main/java/com/ecomapp/productservice/controllers/ProductController.java
+++ src/main/java/com/ecomapp/productservice/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.ecomapp.productservice.controllers;
 
 import com.ecomapp.productservice.models.Product;
 import com.ecomapp.productservice.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,6 +11,17 @@ import java.util.ArrayList;
 @RequestMapping("/product")
 public class ProductController {
     private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/{id}")
+    public Product getSingleProduct(@PathVariable long id) {
+        return productService.getSingleProduct(id);
+    }
+
     @GetMapping()
     public ArrayList<Product> getAllProducts() {
         ArrayList products = new ArrayList<>();
--- src/main/java/com/ecomapp/productservice/services/FakeStoreProductService.java
+++ src/main/java/com/ecomapp/productservice/services/FakeStoreProductService.java
@@ -1,12 +1,35 @@
 package com.ecomapp.productservice.services;
 
+import com.ecomapp.productservice.DTOs.FakeStoreProductDTO;
+import com.ecomapp.productservice.models.Category;
 import com.ecomapp.productservice.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 public class FakeStoreProductService implements ProductService {
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public FakeStoreProductService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    private Product convertToProduct(FakeStoreProductDTO fakeStoreProductDTO) {
+        Product product = new Product();
+        product.setId(fakeStoreProductDTO.getId());
+        product.setTitle(fakeStoreProductDTO.getTitle());
+        product.setPrice(fakeStoreProductDTO.getPrice());
+        product.setDescription(fakeStoreProductDTO.getDescription());
+        product.setCategory(new Category());
+        product.getCategory().setTitle(fakeStoreProductDTO.getCategory());
+        product.setImageURL(fakeStoreProductDTO.getImage());
+
+        return product;
+    }
 
     @Override
     public List<Product> getAllProducts() {
@@ -15,7 +38,11 @@ public class FakeStoreProductService implements ProductService {
 
     @Override
     public Product getSingleProduct(Long id) {
-        return null;
+        FakeStoreProductDTO fakeStoreProductDTO = restTemplate.getForObject(
+                "https://fakestoreapi.com/products/" + id,
+                FakeStoreProductDTO.class);
+
+        return convertToProduct(fakeStoreProductDTO);
     }
 
     @Override
