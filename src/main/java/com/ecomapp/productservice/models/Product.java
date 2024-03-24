752917a048f4c063a2a76c5508a2b63e8a958e40
sivaram045 <sivaram045@gmail.com>
2024-03-25 05:19:23 +0530

    importance of jakarta.persistence --> CascadeType while using cardinality

--- src/main/java/com/ecomapp/productservice/models/Product.java
+++ src/main/java/com/ecomapp/productservice/models/Product.java
@@ -15,7 +15,9 @@ public class Product extends BaseModel{
     private String title;
     private Double price;
     private String description;
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL) //since a category must be created before a product,
+                                          // not using cascade will give an error..!
+                                          // while inserting a product into the database.
     private Category category;
     private String imageURL;
 }
